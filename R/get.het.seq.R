33e757e13a21703708cbbecc61e37ab78a93c47d
dvchene <duchene>
2016-05-29 19:05:51 +1000

    updated functions

---
+++ R/get.het.seq.R
@@ -0,0 +1,24 @@
+# This function requires the function in this repository get.seq.
+require(phangorn)
+
+get.het.seq <- function(ntax = 36, backbone.ntax = 10, trlen = 3, backbone.trlen = 1.5, slen = 1000){
+    tr1 <- rtree(backbone.ntax)
+    tr1$edge.length <- rexp(length(tr1$edge.length))
+    tr1$edge.length <- tr1$edge.length/sum(tr1$edge.length) * backbone.trlen
+    s1 <- get.seq(tr1, model = "gtr.g", l = slen)
+    
+    tr2 <- rtree(ntax - backbone.ntax)
+    tr2$edge.length <- rexp(length(tr2$edge.length))
+    tr2$edge.length <- tr2$edge.length/sum(tr2$edge.length) * (trlen - backbone.trlen)
+    s2 <- get.seq(tr2, model = "gtr.g", l = slen, rootseq =  as.character(as.DNAbin(s1)[1, ]))
+
+    tr3 <- bind.tree(tr1, tr2, where = 1)
+    tr3$tip.label <- paste0('t', 1:length(tr3$tip.label))
+
+    names(s1) <- paste0('t', 1:length(s1))
+    names(s2) <- paste0('t', length(s1):(length(s1)-1 + length(s2)))
+
+    s3 <- rbind(as.DNAbin(s1)[-length(s1), ], as.DNAbin(s2))
+    
+    return(list(tr3, s3))
+}
