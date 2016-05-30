872b06b38d339a333c0c2760a227456a91484e8f
Sebastian Duchene <sebastian.duchene@sydney.edu.au>
2016-05-30 15:09:02 +1000

    complted run.phyml.bootstrap;

--- R/run.phyml.bootstrap.R
+++ R/run.phyml.bootstrap.R
@@ -19,7 +19,7 @@ run.phyml.bootstrap <- function(phyml_path, file_name, subs_model, n_reps, n_pro
             return(stats)
         }else if(model == 'JC'){
             system(paste(phymlPath, '-i', data_name, '-m 000000 -a 1 -c 1'))
-            tree <- readLines(grep(paste0(rep_name, '_phyml_tree'), x = dir(), value = T))
+            tree <- readLines(grep(paste0(data_name, '_phyml_tree'), x = dir(), value = T))
             names(tree) <- 'tree'
             return(tree)
         }else{
@@ -30,13 +30,15 @@ run.phyml.bootstrap <- function(phyml_path, file_name, subs_model, n_reps, n_pro
     get_bootstrap_replicate <- function(phymlPath, nuc_data, rep_name, subs_model){
         sample_data <- nuc_data[, sample(x = 1:ncol(nuc_data), size = ncol(nuc_data), replace = T)]
         write.dna(sample_data, file = rep_name, format = 'sequential', nbcol = -1, colsep = '')
-        return(run_phyml(phymlPath, data_name = rep_name, model = subs_model))
+        out <- run_phyml(phymlPath, data_name = rep_name, model = subs_model)
+        system(paste0('rm ', rep_name, '*'))
+        return(out)
     }
 
     cl <- makeCluster(n_proc)
     registerDoParallel(cl)
 
-    reps <- foreach(x = 1:n_reps, .packages = 'ape', .combine = list) %dopar% get_bootstrap_replicate(phyml_path, nuc_data = nuc_data, rep_name = paste0('rep', x), subs_model = subs_model)
+    reps <- foreach(x = 1:n_reps, .packages = 'ape') %dopar% get_bootstrap_replicate(phyml_path, nuc_data = nuc_data, rep_name = paste0('rep', x), subs_model = subs_model)
 
     rbind_list <- function(data_list){
     if(length(data_list) == 1){
@@ -47,21 +49,26 @@ run.phyml.bootstrap <- function(phyml_path, file_name, subs_model, n_reps, n_pro
         rbind(data_list[[1]], rbind_list(data_list[-1]))
     }
 }
-   out_result <- rbind_list(reps)
+    out_log <- rbind_list(reps)
+    if(subs_model == 'GTR+G'){
+        colnames(out_log) <- c('ac', 'ag', 'at', 'cg', 'ct', 'gt', 'a', 'c', 'g', 't', 'alpha', 'tree')
+        write.table(out_log[, -ncol(out_log)], file = paste0(file_name, '_ml_bootstrap.log'), row.names = F, quote = F)
+        writeLines(text = out_log[,ncol(out_log)], con = paste0(file_name, '_ml_bootstrap.trees'), sep = '\n')
+    }else{
+        writeLines(text = out_log, con = paste0(file_name, '_ml_bootstrap.trees'), sep = '\n')
+    }
+    out_log
 }
 
 
-
-true_data <- as.DNAbin(simSeq(rtree(10)))
-write.dna(true_data, file = 'test_true_data.phy', format = 'sequential', nbcol = -1, colsep = '')
-
-phyml_path <- '~/Desktop/phylo_programs/PhyML-3.1/PhyML-3.1_macOS-MountainLion'
-file_name <- 'test_true_data.phy'
-subs_model <- 'GTR+G'
-n_reps <- 50
-n_proc <- 5
-
-t1 <- run.phyml.bootstrap(phyml_path, 'test_true_data.phy', subs_model = 'GTR+G', n_reps = 10, n_proc = 5)
+#true_data <- as.DNAbin(simSeq(rtree(10)))
+#write.dna(true_data, file = 'test_true_data.phy', format = 'sequential', nbcol = -1, colsep = '')
+#phyml_path <- '~/Desktop/phylo_programs/PhyML-3.1/PhyML-3.1_macOS-MountainLion'
+#file_name <- 'test_true_data.phy'
+#subs_model <- 'GTR+G'
+#n_reps <- 50
+#n_proc <- 5
+#t1 <- run.phyml.bootstrap(phyml_path, 'test_true_data.phy', subs_model = 'GTR+G', n_reps = 5, n_proc = 5)
 
 
 
