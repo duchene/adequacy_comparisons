94c083187e5d7910554598d4ca9719f5ee115e5b
duchene <duchenedavid@gmail.com>
2016-06-10 09:13:55 +1000

    added analyses

---
+++ stationary/statshort.5.ML.jc_folder/run.jc.phyml.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -N runname
+#PBS -l ncpus=1
+#PBS -l walltime=2:00:00
+#PBS -l pmem=4gb
+#PBS -P RDS-FSC-Phylogenomics-RW
+#PBS -q compute
+cd $PBS_O_WORKDIR
+module load R
+module load phyml
+Rscript run.jc.phyml.Rscript
