725ca6c03161c7e3e0eb556d160efa816bf44ac9
dvchene <duchene>
2016-06-19 13:16:28 +1000

    added adeq results

--- stationary/statlong.3.ML.jc_folder/run.jc.phyml.sh
+++ stationary/statlong.3.ML.jc_folder/run.jc.phyml.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -N runname
 #PBS -l ncpus=1
-#PBS -l walltime=2:00:00
-#PBS -l pmem=4gb
+#PBS -l walltime=5:00:00
+#PBS -l mem=4GB
 #PBS -P RDS-FSC-Phylogenomics-RW
 #PBS -q compute
 cd $PBS_O_WORKDIR
